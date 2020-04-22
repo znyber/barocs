d65d14a3df21084e89c509daef6fc9fbc95f75b0
znyber <pro_army@live.com>
2020-04-22 18:52:21 +0700

    litle

--- bar_ocs.sh
+++ bar_ocs.sh
@@ -73,7 +73,7 @@ echo "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge cont
 wget http://www.webmin.com/jcameron-key.asc
 apt-key add jcameron-key.asc
 apt update && apt upgrade
-apt install dropbear stunnel squid sslh python3-pip python3 simple-obfs shadowsocks-libev webmin libxml-parser-perl openssl -y
+apt install dropbear stunnel squid sslh python3-pip python3 simple-obfs shadowsocks-libev webmin libxml-parser-perl openssl  apache2-utils -y
 sed -i '10s/.*/ssl=0/' /etc/webmin/miniserv.conf
 mboh=$(dig @resolver1.opendns.com ANY myip.opendns.com +short)
 echo 'RUN=yes' > /etc/default/sslh
@@ -313,3 +313,4 @@ port		: 7300
 -----------------------------------------
 EOF
 echo $mboh
+cat $mboh
