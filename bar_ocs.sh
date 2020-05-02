6b1b5f23558bedcbbff4f50789814af2876a7d8c
znyber <pro_army@live.com>
2020-05-02 22:09:54 +0700

    update zssh pptp

--- bar_ocs.sh
+++ bar_ocs.sh
@@ -1,31 +1,22 @@
 #!/bin/bash
-apt install python3-pip -y
+apt install python3-pip git net-tools -y
+apt install apache2 apache2-bin apache2-data apache2-utils libapr1 libaprutil1 libaprutil1-dbd-sqlite3 libaprutil1-ldap libconfig9 libfile-copy-recursive-perl liblua5.2-0 ssl-cert update-inetd
 mkdir -p /var/www/html
+wget 
 cd /var
-mboh=$(dig @resolver1.opendns.com ANY myip.opendns.com +short)
-git clone https://github.com/znyber/wg_config.git
+mboh=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+git clone git@github.com:znyber/wg_config.git
 chmod -R 777 wg_config
 cd wg_config
 rsync -avz -P script/* /usr/bin/
 pwd
 pip3 install flask
 read -p "Press enter to continue"
-./wireguard-install.sh
+./user.sh
 read -p "Press enter to continue"
 ./openvpn-install.sh
 read -p "Press enter to continue"
-SERVER_PRIV_KEY=$(wg genkey)
-SERVER_PUB_KEY=$(echo "$SERVER_PRIV_KEY" | wg pubkey)
-mboh=$(dig @resolver1.opendns.com ANY myip.opendns.com +short)
-patokan=$(echo "\$_SERVER_PORT")
-cat <<EOF > /var/wg_config/wg.def
-_INTERFACE=wg0
-_VPN_NET=10.76.0.0/23,fd42:42:42::1/64
-_SERVER_PORT=7676
-_SERVER_LISTEN=$mboh:$patokan
-_SERVER_PUBLIC_KEY=$SERVER_PRIV_KEY
-_SERVER_PRIVATE_KEY=$SERVER_PUB_KEY
-EOF
+mboh=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
 
 cat <<EOF > /etc/openvpn/server/server.conf
 port 445
@@ -75,30 +66,54 @@ status openvpn-status.log
 verb 3
 EOF
 
-./user.sh -a oesman
 echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
 echo "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib" >> /etc/apt/sources.list
 wget http://www.webmin.com/jcameron-key.asc
 apt-key add jcameron-key.asc
 apt update && apt upgrade
-apt install dropbear 
-apt install stunnel 
-apt install squid 
-apt install sslh 
-apt install python3-pip 
-apt install python3 
-apt install simple-obfs 
-apt install shadowsocks-libev
-apt install webmin
-apt install libxml-parser-perl
-apt install openssl
-apt install apache2-utils
+apt install -y dropbear 
+apt install -y stunnel 
+apt install -y squid 
+apt install -y python3-pip 
+apt install -y python3 
+apt install -y simple-obfs 
+apt install -y shadowsocks-libev
+apt install -y webmin
+apt install -y libxml-parser-perl
+apt install -y openssl
+apt install -y apache2-utils
+apt install -y pptpd
+
+cat <<EOF > /etc/ppp/pptpd-options
+name pptpd
+refuse-pap
+refuse-chap
+refuse-mschap
+require-mschap-v2
+require-mppe-128
+ms-dns 10.7.0.1
+ms-dns 10.8.0.1
+proxyarp
+nodefaultroute
+lock
+nobsdcomp
+novj
+novjccomp
+nologfd
+EOF
+
+cat <<EOF > /etc/ppp/chap-secrets
+znyber pptpd zxc *
+EOF
+systemctl enable pptpd
+echo "localip 172.76.0.1" >> /etc/pptpd.conf
+echo "remoteip 172.76.0.2-254" >> /etc/pptpd.conf
 sed -i '10s/.*/ssl=0/' /etc/webmin/miniserv.conf
-mboh=$(dig @resolver1.opendns.com ANY myip.opendns.com +short)
-cat <<EOF > /etc/default/sslh
+mboh=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+cat <<EOF > /etc/default/znyber
 RUN=yes
-DAEMON=/usr/sbin/sslh
-DAEMON_OPTS="--user sslh --listen  $mboh:443 --ssh 0.0.0.0:444 --openvpn 0.0.0.0:445 --ssl 0.0.0.0:143 --ssl 0.0.0.0:3129 --ssl 0.0.0.0:990 --http 127.0.0.1:3128 --pidfile /var/run/sslh/sslh.pid"
+DAEMON=/usr/sbin/znyber
+DAEMON_OPTS="--user znyber --listen  $mboh:443 --ssh 0.0.0.0:444 --openvpn 0.0.0.0:445 --ssl 0.0.0.0:143 --ssl 0.0.0.0:3129 --ssl 0.0.0.0:990 --http 127.0.0.1:3128 --pidfile /var/run/znyber/znyber.pid"
 
 EOF
 echo 'NO_START=0' > /etc/default/dropbear
@@ -186,7 +201,7 @@ refresh_pattern . 0 20% 4320
 visible_hostname Daybreakersx
 EOF
 
-apt install stunnel
+apt install -y stunnel
 cat <<EOF > /etc/stunnel/stunnel.conf
 cert = /etc/stunnel/stunnel.pem
 client = no
@@ -207,9 +222,58 @@ accept = 3129
 connect = 127.0.0.1:3128
 EOF
 
-openssl genrsa -out key.pem 2048
-openssl req -new -x509 -key key.pem -out cert.pem -days 1095
-cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
+cat <<EOF > /etc/stunnel/stunnel.pem
+-----BEGIN RSA PRIVATE KEY-----
+MIIEogIBAAKCAQEAz/IrnCgwE3McZQqFjSdoP2VGW8dGNbgoifc2jgAJOdTkkuoX
+qFiV/91kUKTsjnHPo8nBmMQ/ZshgS6d0v2G4Z+ar3j7LBJAXUegFYafi6aiY0gIA
+6KVpA0NVK4WYptjJMNke4z00usZXoZROCHxewmXUNVIabtAGtFV7acBh6qAHAlSC
+i3kfphWimy/joB1rvacDr8AyUSMurO/Odb8z5maodL1KhBls41TWBa9FNqF8Kk6S
+sjdB1nTUgMPZgEYWj/mK4mCpeICdADKj6qTTl8huPax5MI4WHFKPWPD2sYJd2Sh3
+UF6ql7wa1Towez5FN9MXZ40HLg72g/ubiCGAlwIDAQABAoIBADyhyw90NFhndv8L
+K1e2BtJ9UB+QoislsERSHckXv5jqN1+S/CTs94esYQSWohcCnl2OhXFqv2XTxSgq
+AoOPFbrN6o0Z2A5TiUkrku4fiq1AqJaLWQJ8wHrgFiRjhtgXW8pvdG8juI1BKeoG
+GR6mTOb31AFcGDL5WwsX5a03GBXV3nh1e1bWI2Ble8hGHMR58jyIO4YOe/BaKWFl
+9MMdeZ3IqS/8A7b+wFWi7zfwpSh2NS/QuxxmwSBcFbWb4uQ9la9gnabPCTS9Uujp
+tkkBeLSy4L62lP6Fal8RNtGe86Afq6G8vHJ2lGIOs5HWJ27n981+bwRfC75BpcVm
+dHX/dokCgYEA9wFzMOPQ9WtyGpcSWaXdt2+mrNI3qS+rnjICRBxCB2mg1z/nJg7v
+QqC1YAIglgC6zclBP06aDk3IaKIVg4t7bvaf6joi5OYKOILzVo9rhnfRvI1FRUMj
+R6k0PGuZWr9eyQNwnx3+jPpSI7EPJtIviRXzkeF6uo7YayO5gXJe4eUCgYEA14Sc
+n2tTF50be+91eab9y7dH3T69mb63ubHZspRMQI1oajdhElF4JMMpb9byn5oQdQpV
+4kqTw0sxUSWz+BipdVWqUPeYSj+puHGqAVi1X4eiRlUWAjJuo643qKtFquHfC+Z/
+21DJMGbw+GFGVjxgijI2zve+I459FBAuraiM4MsCgYBF6QYmX0SEQd3wwrNx5i1D
+vvkozvLsEs77HmJfzrOtj2rzoWWV8wIrFHthahi4/AnjNsQd2+xkggVoAAAg1Dii
+ZnFKhRvdN3LLHd0rsboYPA+D02Q4v1Db9nsPYtDry2BTeoLEtmOWmT7oVTPZpAHK
+RglDT9NBvC5q3Jijl7BdNQKBgEWDHklFAo3xPHYi7A8Kz30vZlxz6g92tieMt5yj
+vFxtv6nSptqTsF6bG/hYqfTUak51wPG2ACbTT9rOgFebDL0tnjtzQIliM0msT5sT
+k5x2UN7CftDCl3DvtkAOze+p3pkmBUMlsV6NNVfHeDpESxcHzym+OnOZd7pK5eNZ
+I+c1AoGAM/zBM2L95F6BtUNVKcjWsWh8aXW6o/BeAg77GEq5gKfYo1HjPEiPyubn
+O2iRbiPNPf6nHUW3bCj9dhfjKZaIxUu5lOhymqQL7vbTFyTCj5KkW7U7by1IKMay
+2dXpBDq+ouMdGqQ9MwUoxtkfNo5I7h2DOUj+NASTfBTavfaaV3Y=
+-----END RSA PRIVATE KEY-----
+-----BEGIN CERTIFICATE-----
+MIID2TCCAsGgAwIBAgIUZCqSVAjF1/xWqHAtbuRf2ZIf2uwwDQYJKoZIhvcNAQEL
+BQAwfDELMAkGA1UEBhMCSUQxFDASBgNVBAgMC2phdmEtY2VudGVyMRMwEQYDVQQH
+DApwdXJ3b2tlcnRvMSEwHwYDVQQKDBhJbnRlcm5ldCBXaWRnaXRzIFB0eSBMdGQx
+HzAdBgkqhkiG9w0BCQEWEHVzbWFuQHpueWJlci5jb20wHhcNMjAwNTAyMTQyMjAz
+WhcNMjMwNTAyMTQyMjAzWjB8MQswCQYDVQQGEwJJRDEUMBIGA1UECAwLamF2YS1j
+ZW50ZXIxEzARBgNVBAcMCnB1cndva2VydG8xITAfBgNVBAoMGEludGVybmV0IFdp
+ZGdpdHMgUHR5IEx0ZDEfMB0GCSqGSIb3DQEJARYQdXNtYW5Aem55YmVyLmNvbTCC
+ASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAM/yK5woMBNzHGUKhY0naD9l
+RlvHRjW4KIn3No4ACTnU5JLqF6hYlf/dZFCk7I5xz6PJwZjEP2bIYEundL9huGfm
+q94+ywSQF1HoBWGn4umomNICAOilaQNDVSuFmKbYyTDZHuM9NLrGV6GUTgh8XsJl
+1DVSGm7QBrRVe2nAYeqgBwJUgot5H6YVopsv46Ada72nA6/AMlEjLqzvznW/M+Zm
+qHS9SoQZbONU1gWvRTahfCpOkrI3QdZ01IDD2YBGFo/5iuJgqXiAnQAyo+qk05fI
+bj2seTCOFhxSj1jw9rGCXdkod1Beqpe8GtU6MHs+RTfTF2eNBy4O9oP7m4ghgJcC
+AwEAAaNTMFEwHQYDVR0OBBYEFDv8q1oThLPw77PIct0KQcu2eHGvMB8GA1UdIwQY
+MBaAFDv8q1oThLPw77PIct0KQcu2eHGvMA8GA1UdEwEB/wQFMAMBAf8wDQYJKoZI
+hvcNAQELBQADggEBAFlrojs7TBahQPUzRYRkfIa7XO16VwlNndDVD1OaszBxNByO
+hHsO2/byEnnG277Ebmb31kufccG9itAoieWkWc5CpFNFQjQJtH5G9ochg0JkdQhB
+JDqNYnCYhT3swFOV6BjNuIjj9j3/neOwyqekTA6Cy+KczOvDfTPAaHZGCE9tXMrw
+WNhPYjj7IM5AbZiEuOeCZb0Ymg/rVm7QNdGGdJ2MfnCGPxlp+UVj/nemDXCQoC1R
+4ZqzF+wnSDEVGSfNe27ee4PyLZTXKbvZAkwrAtQxPKWU83FZ/wkJwO0MW1sKBVAt
+lLG3Pg5I1Kam30B0NW/U5j/FYO4QCY9dDKOCe6M=
+-----END CERTIFICATE-----
+EOF
 sed -i '6s/.*/ENABLED=1/' /etc/default/stunnel4
 read -p "Press enter to continue"
 
@@ -279,7 +343,7 @@ service bv7300 status
 systemctl disable apache2.service
 service openvpn-server@server restart
 service openvpn-server@server-udp restart
-read -p "Press enter to continue"
+#read -p "Press enter to continue"
 
 ./pihole
 pihole -a -p zxc
@@ -291,13 +355,11 @@ sysctl -p
 service lighttpd restart
 service wg-quick@wg0 restart
 service dropbear restart
-service sslh restart
 service stunnel4 restart
-/etc/init.d/sslh restart
 /etc/init.d/webmin restart
 /etc/init.d/squid restart
 CERT=$(cat /etc/openvpn/server/ca.crt)
-mboh=$(dig @resolver1.opendns.com ANY myip.opendns.com +short)
+mboh=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
 cat <<EOF > /var/www/html/client.ovpn
 client
 dev tun
@@ -331,7 +393,7 @@ verb 2
 $CERT
 </ca>
 EOF
-mboh=$(dig @resolver1.opendns.com ANY myip.opendns.com +short)
+mboh=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
 jeneng=$(hostname)
 cat <<EOF > $mboh.txt
 -----------------------------------------
@@ -369,11 +431,21 @@ password    [success=1 default=ignore]  pam_unix.so obscure sha512 minlen=1
 password        requisite                       pam_deny.so
 password        required                        pam_permit.so
 EOF
-
+service lighttpd restart
+service wg-quick@wg0 restart
+service dropbear restart
+service sslh restart
+service stunnel4 restart
+systemctl start pptpd
+/etc/init.d/webmin restart
+/etc/init.d/squid restart
 echo "/bin/false" >> /etc/shells
 echo $mboh
 cat $mboh.txt
 ufw disable
+echo "wait..."
+sleep 20
+service znyber restart
 read -p "Press enter to continue"
-netstat -netulp |grep "8099\|5000\|10000\|8767\|22\|443\|444\|143\|990\|3129\|80\|8080\|445\|7200\|7300\|7676"
+netstat -netulp |grep "8099\|5000\|10000\|8767\|22\|443\|444\|143\|990\|3129\|80\|8080\|445\|7200\|7300"
 echo "pastikan sama dengan yang di atas"
